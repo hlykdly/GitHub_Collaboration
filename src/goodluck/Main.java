17ac56f08c9b54fd59257b5740839eb0c0dabdff
hlykdly <mayamoto@protonmail.com>
2022-01-20 16:32:53 +0100

    Method and new person object

--- src/goodluck/Main.java
+++ src/goodluck/Main.java
@@ -3,6 +3,9 @@ package goodluck;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        Person person = new Person("Peter", "Forsberg", 45);
+        person.speak();
+
     }
 }
