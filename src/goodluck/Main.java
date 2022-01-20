eaacd24bcee70197981e5121cd763939ba34931a
frykj <fryk89@gmail.com>
2022-01-20 16:34:23 +0100

    Uppgift 4

--- src/goodluck/Main.java
+++ src/goodluck/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+Person p1 = new Person("Johannes", "Fryk", 32);
+
+
+        System.out.println(p1.speak());
+
     }
 }
