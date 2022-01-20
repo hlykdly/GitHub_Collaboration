830182ae427ce19403a1d2a524d9285d7d089a07
frykj <fryk89@gmail.com>
2022-01-20 16:20:08 +0100

    Person

---
+++ src/goodluck/Person.java
@@ -0,0 +1,13 @@
+package goodluck;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int Age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Age = age;
+    }
+}
